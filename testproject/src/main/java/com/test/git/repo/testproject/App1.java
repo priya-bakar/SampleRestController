3deb92f6330756fca261e4a86bc1069a0eadabe5
PriyaG1 <priya@talentturbo.com>
2020-09-08 12:45:49 -0400

    changes from hi to everyone done

--- testproject/src/main/java/com/test/git/repo/testproject/App1.java
+++ testproject/src/main/java/com/test/git/repo/testproject/App1.java
@@ -2,6 +2,6 @@ package com.test.git.repo.testproject;
 
 public class App1 {
 	public static void main(String[] args) {
-		System.out.println("Hello World &&&&&& Hi Everyone");
+		System.out.println("Hello World &&&&&& Hello Everyone");
 	}
 }
